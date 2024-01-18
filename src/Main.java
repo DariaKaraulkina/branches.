9b92dbc6ef04f383d192bea78edfa28224a5fe4d
DariaKaraulkina <D.morgunovaa@mail.ru>
2024-01-18 13:58:29 +0300

    sout2, bugfix

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        bugfix();
+        System.out.println("Это код из ветки <sout1>");
         System.out.println("Hello world!");
+        System.out.println("Баг пофиксили, ура!");
+    }
+    public static void bugfix() {
+        System.out.println("Этот код фиксит найденный баг");
     }
 }
